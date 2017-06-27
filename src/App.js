0a6f02566e6816255822817f98480b4820f4cb83
suraj <suraj@suraj.com>
2017-06-27 12:34:14 +0530

    moved api call to App

--- src/App.js
+++ src/App.js
@@ -2,8 +2,23 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import NewsGrid from './components/newsgrid'
+import * as HNapi from './api/apicall'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    // var topNewsArray = HNapi.getTopNews();
+    // console.log('topNewsArray:'+topNewsArray);
+    HNapi.getTopNews()
+    .then(function(data) {
+      var topNewsArray = data;
+      console.log('TopNewsArray in App:'+topNewsArray);
+    })
+    .catch(function(error) {
+      console.log(error);
+    })
+  }
+
   render() {
     return (
       <div className='newsgrid-div'>
