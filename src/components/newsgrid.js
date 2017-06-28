73fb9af6c168a1adca63a96411f0ab5a258b6866
suraj <suraj@suraj.com>
2017-06-28 11:52:06 +0530

    done for newsgrid

--- src/components/newsgrid.js
+++ src/components/newsgrid.js
@@ -33,14 +33,17 @@ componentWillReceiveProps(nextProps){
 
   render() {
     var rows = [];
-    for(var i=0; i<this.state.newsItemArray.length; i++) {
-      
+    var tempArray = this.state.newsItemArray;
+    for(var i=0; i<tempArray.length; i++) {
+      var ary = new Array();
+      ary.push(tempArray[i]);
+      ary.push(tempArray[i+1]);
+      ary.push(tempArray[i+2]);
+      rows.push(<NewsRow newsRowItems={ary} key={i} />)
     }
     return(
       <div>
-        <NewsRow />
-        <NewsRow />
-        <NewsRow />
+        {rows}
       </div>
     );
   }
