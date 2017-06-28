bf59f271e1fd9a1a6ad6691d212311d8c8b7d544
suraj <suraj@suraj.com>
2017-06-28 12:06:20 +0530

    done for newitem, but doesnt work

--- src/components/newsitem.js
+++ src/components/newsitem.js
@@ -10,9 +10,27 @@ class NewsItem extends React.Component {
   render() {
     return(
       <div className='col-md-4'>
-      <Jumbotron bsStyle='primary'>
-      
-      </Jumbotron>
+      <Jumbotron bsStyle='primary' >
+        <h2>{this.props.newsItem.title}</h2>
+        <hr/>
+        <div className="row">
+          <div className = "col-md-4">
+              <i className="material-icons">person</i>
+              <br/>
+              {this.props.newsItem.by}
+          </div>
+          <div className = "col-md-4">
+              <i className="material-icons">star_rate</i>
+              <br/>
+              {this.props.newsItem.score}
+          </div>
+          <div className = "col-md-4">
+              <i className="material-icons">comment</i>
+              <br/>
+              {this.props.newsItem.descendants}
+          </div>
+        </div>
+        </Jumbotron>
       </div>
     )
   }
