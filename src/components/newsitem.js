207fbf29ea8eef6962a0f607c484e95db2c10c96
suraj <suraj@suraj.com>
2017-06-28 15:10:57 +0530

    removed state from newsrow and newsitem, works as expected

--- src/components/newsitem.js
+++ src/components/newsitem.js
@@ -5,36 +5,34 @@ import * as HNapi from '../api/apicall'
 class NewsItem extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      newsItem : {}
-    }
+
   }
 
   componentWillReceiveProps(nextProps) {
-    this.setState({ newsItem: nextProps.newsItem });
+    this.props = nextProps;
   }
 
   render() {
     return(
       <div className='col-md-4'>
       <Jumbotron bsStyle='primary' >
-        <h2>{this.state.newsItem.title}</h2>
+        <h2>{this.props.newsItem.title}</h2>
         <hr/>
         <div className="row">
           <div className = "col-md-4">
               <i className="material-icons">person</i>
               <br/>
-              {this.state.newsItem.by}
+              {this.props.newsItem.by}
           </div>
           <div className = "col-md-4">
               <i className="material-icons">star_rate</i>
               <br/>
-              {this.state.newsItem.score}
+              {this.props.newsItem.score}
           </div>
           <div className = "col-md-4">
               <i className="material-icons">comment</i>
               <br/>
-              {this.state.newsItem.descendants}
+              {this.props.newsItem.descendants}
           </div>
         </div>
         </Jumbotron>
